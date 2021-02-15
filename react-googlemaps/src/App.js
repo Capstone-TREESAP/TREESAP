8e4292f26f61c5634b1cbbe19fa2c54d1ea934a3
Caelin <prestoncaelin@gmail.com>
2021-02-14 17:06:09 -0800

    ReactJS project with google maps API integration

---
+++ react-googlemaps/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: 49.2367,
+            lng: -123.2031
+          }
+        }
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyB8xmip8bwBsT_iqZ2-jBei-gwKNm5kR3A'
+})(MapContainer);
