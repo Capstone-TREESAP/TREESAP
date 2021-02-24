c7796ffe59de503446792d45ae0a39c5ec0c5c6f
caelinp <31804667+caelinp@users.noreply.github.com>
2021-02-23 21:14:56 -0800

    Update App.js

--- react-googlemaps/src/App.js
+++ react-googlemaps/src/App.js
@@ -79,5 +79,5 @@ export class MapContainer extends Component {
 
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyB8xmip8bwBsT_iqZ2-jBei-gwKNm5kR3A'
+  apiKey: 'YOUR_API_KEY_HERE'
 })(MapContainer);
