a1b565dc2a3e6026c04f88619647b50f11278219
Shaylin Cattell <shaylincattell@gmail.com>
2021-03-14 14:40:25 -0700

    Adding ability to edit and delete polygons

--- react-googlemaps/src/App.js
+++ react-googlemaps/src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, useReducer, useState } from 'react';
+import ReactDOM from 'react-dom';
 import { Map, GoogleApiWrapper, Polygon, Marker, InfoWindow } from 'google-maps-react';
 import polygons from './lidar_polygons_with_area.json';
 import SettingsView from './settings';
@@ -28,12 +29,46 @@ function parsePolygons(polygons){
       {
         "id": i,
         "points": points,
-        "area": area
+        "area": area,
+        "editable": false
       }
     )
   }
 };
 parsePolygons(polygons);
+
+function createEditablePolygon(props, polygon, map) {
+  const {google} = props
+
+  const polygonEdit = new google.maps.Polygon({
+    draggable: true,
+    editable: true,
+    fillColor: "#014421",
+    fillOpacity: 0.65,
+    paths: polygon.points,
+    strokeColor: "#014421",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    map: map
+  })
+
+  return polygonEdit;
+}
+
+function getPolygonEdits(editablePolygon) {
+  let path = editablePolygon.getPath();
+
+  let points = [];
+  for (var i = 0; i < path.length; i++) {
+    points.push(path.getAt(i))
+  }
+
+  return points
+}
+
+function removeEditablePolygon(editablePolygon) {
+  editablePolygon.setMap(null)
+}
 
 const mapStyles = {
   width: '100%',
@@ -46,7 +81,8 @@ export class MapContainer extends Component {
     showInfoWindow: false, //Whether a polygon info window is shown
     clickedLocation: null,
     marker: null,
-    polygon: null
+    polygon: null,
+    editPolygon: null
   };
 
   //Functions for calculating ecosystem services
@@ -71,6 +107,8 @@ export class MapContainer extends Component {
 
   handleClick = 
   (polygon, map, coords) => {
+    this.makeCurrentPolygonUneditable();
+
     this.setState({
       clickedLocation: coords,
       showInfoWindow: this.state.showInfoWindow,
@@ -84,27 +122,76 @@ export class MapContainer extends Component {
       showDrawingView: true
     })
   }
+
+  makePolygonEditable = (polygon) => {
+    let index = collectedPolygons.findIndex(element => element === polygon)
+    collectedPolygons.splice(index, 1);
+    let editablePolygon = createEditablePolygon(this.props, polygon, this._map.map);
+
+    this.setState({
+      editPolygon: editablePolygon
+    })
+  }
+
+  makeCurrentPolygonUneditable = () => {
+    if (this.state.editPolygon == null) {
+      return
+    }
+
+    let newPoints = getPolygonEdits(this.state.editPolygon)
+    removeEditablePolygon(this.state.editPolygon)
+
+    this.state.polygon.points = newPoints
+
+    collectedPolygons.push(this.state.polygon)
+
+    this.setState({
+      editPolygon: null
+    })
+  }
+
+  deletePolygon = (polygon) => {
+    let index = collectedPolygons.findIndex(element => element === polygon)
+    collectedPolygons.splice(index, 1)
+
+    this.setState({
+      polygon: null,
+      clickedLocation: null
+    })
+  }
   
   //Display a set of polygons
-  displayPolygons = () => collectedPolygons
-  .map(polygon => 
-  <Polygon
-    paths={polygon.points}
-    key={polygon.id}
-    onClick={(t, map, coords) => 
-      this.handleClick(polygon, map, coords.latLng)
+  displayPolygons = () => {
+    return collectedPolygons.map(polygon => 
+      <Polygon
+        paths={polygon.points}
+        key={polygon.id}
+        onClick={(t, map, coords) => 
+          this.handleClick(polygon, map, coords.latLng)
+        }
+        strokeColor="#014421"
+        strokeOpacity={0.8}
+        strokeWeight={2}
+        fillColor="#014421"
+        fillOpacity={0.65}
+        editable={polygon.editable}
+      />);
     }
-    strokeColor="#014421"
-    strokeOpacity={0.8}
-    strokeWeight={2}
-    fillColor="#014421"
-    fillOpacity={0.65}
-  />)
+
+  onInfoWindowOpen(polygon) {
+    const buttons = (<div>
+      <button type="button">Report</button>
+      <button type="button" onClick={() => {this.makePolygonEditable(polygon); this.onClose();}}>Edit</button>
+      <button type="button" onClick={() => {this.deletePolygon(polygon); this.onClose();}}>Remove</button>
+    </div>)
+    ReactDOM.render(React.Children.only(buttons), document.getElementById("iwc"))
+  }
 
   render() {
     return (
       <Map
         google={this.props.google}
+        ref={(map) => this._map = map}
         zoom={14}
         style={mapStyles}
         initialCenter={
@@ -124,15 +211,16 @@ export class MapContainer extends Component {
           visible={this.state.showInfoWindow}
           marker={this.state.marker}
           onClose={this.onClose}
+          onOpen={() => {
+            this.onInfoWindowOpen(this.state.polygon)
+          }}
         >
-          <>
-            <div>
-              <h3>Area: {this.state.polygon ? this.state.polygon.area : null} m<sup>2</sup></h3>
-              <h3> Carbon sequestered: {this.state.polygon ? this.getCarbonSequesteredAnnually(this.state.polygon.area).toFixed(2) : null} tonnes/year</h3>
-              <h3> Avoided rainwater run-off: {this.state.polygon ? this.getAvoidedRunoffAnnually(this.state.polygon.area).toFixed(2) : null} litres/year</h3>
-            </div>
-            <button type="button">Report</button>
-          </>
+          <div id="iwc" />
+          <div>
+            <h3>Area: {this.state.polygon ? this.state.polygon.area : null} m<sup>2</sup></h3>
+            <h3> Carbon sequestered: {this.state.polygon ? this.getCarbonSequesteredAnnually(this.state.polygon.area).toFixed(2) : null} tonnes/year</h3>
+            <h3> Avoided rainwater run-off: {this.state.polygon ? this.getAvoidedRunoffAnnually(this.state.polygon.area).toFixed(2) : null} litres/year</h3>
+          </div>
         </InfoWindow>
         <DrawingView 
           showDrawingView={this.state.showDrawingView}
@@ -148,6 +236,6 @@ export class MapContainer extends Component {
 
 //Wrapper for map container
 export default GoogleApiWrapper({
-  apiKey: 'YOUR_API_KEY_HERE',
+  apiKey: '',
   libraries: ['drawing']
 })(MapContainer);
