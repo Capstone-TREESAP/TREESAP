9ccf2c8bd2405094db3aeb207c7492339bc8faed
Rainerino <albertyanyy@gmail.com>
2021-03-18 01:40:11 -0700

    add gui feature. Consider Clustering alternative as the current method is way too slow

--- lidar/lidar/download.py
+++ lidar/lidar/download.py
@@ -1,3 +1,5 @@
+""" Download City of Vancouver LiDAR data and unzip them into a folder. 
+"""
 import requests
 import os
 import zipfile
@@ -16,6 +18,8 @@ TILE = ["4800E_54550N", "4800E_54560N", "4800E_54570N",
 print('Beginning file download with urllib2...')
 
 def download_lidar_dataset():
+    """ Download City of Vancouver LiDAR data
+    """
     for tile in TILE:
         src_url = SRC_URL + tile + ZIP_EXT
         dst_file = DST_DATA + tile + ZIP_EXT
@@ -23,18 +27,30 @@ def download_lidar_dataset():
         download_url(src_url, dst_file)
         
 def download_url(url, save_path, chunk_size=128):
+    """ Download files from given URL
+
+    Args:
+        url (string): source URL
+        save_path (string): file path to save to
+        chunk_size (int, optional): the base chunk of data size. Defaults to 128.
+    """
     r = requests.get(url, stream=True)
     with open(save_path, 'wb') as fd:
         for chunk in r.iter_content(chunk_size=chunk_size):
             fd.write(chunk)
 
 def unzip_files(src_dir):
+    """Unzip files and then remove the zip files. 
+
+    Args:
+        src_dir (string): source directory
+    """
     for root, dirs, files in os.walk(src_dir):
         for file in files:
             if file.endswith(ZIP_EXT):
                 print("Extracting " + os.path.join(root, file))
                 with zipfile.ZipFile(os.path.join(root, file), 'r') as zip_ref:
                     zip_ref.extractall(src_dir)
-                    
+                os.remove(os.path.join(root, file))
 download_lidar_dataset()
 unzip_files(DST_DATA)
---
+++ lidar/lidar/gui.py
@@ -0,0 +1,261 @@
+import plotly.graph_objects as go
+from PIL import Image
+import numpy as np
+from sklearn.cluster import DBSCAN, OPTICS
+from progress.bar import Bar
+import plotly.io as pio
+# too many points, cannot render embedded
+# https://plotly.com/python/renderers/
+
+class GraphGUI():
+    def __init__(self):
+        self._x = np.array([])
+        self._y = np.array([])
+        self._z = np.array([])
+        self._label = np.array([])
+        self.add_image = True
+        self.small_img = Image.open("../data/481E_5456N_tiny.png")
+    @property
+    def x(self):
+        return self._x
+    @property
+    def y(self):
+        return self._y
+    @property
+    def z(self):
+        return self._z
+    @property
+    def label(self):
+        return self._label
+    @x.setter
+    def x(self, x):
+        self._x = x
+    @y.setter
+    def y(self, y):
+        self._y = y
+    @z.setter
+    def z(self, z):
+        self._z = z
+    @label.setter
+    def label(self, label):
+        self._label = label
+        
+    def display_2d_pcd(self, offset, render="browser"):
+        """[summary]
+
+        Args:
+            offset ([type]): [description]
+            render (str, optional): [description]. Defaults to "browser".
+        """
+        if not self.__data_checker(pcd_2d=True):
+            return 
+        pio.renderers.default = render
+        fig = go.Figure(data=go.Scattergl(
+        x = self._x,
+        y = self._y,
+        mode='markers',
+        marker=dict(
+            size=3,
+            colorscale='Viridis',   # choose a colorscale
+            opacity=0.8
+        )))
+
+        if self.add_image:
+            # Add images
+            fig.add_layout_image(
+                    dict(
+                        source=self.small_img,
+                        xref="x",
+                        yref="y",
+                        x=0,
+                        y=offset,
+                        sizex=offset,
+                        sizey=offset,
+                        sizing="stretch",
+                        opacity=0.8,
+                        layer="below")
+            )
+
+        # Set templates
+        fig.update_layout(template="plotly_white")
+
+        fig.update_yaxes(
+            scaleanchor = "x",
+            scaleratio = 1
+        )
+        fig.show()
+        
+    def display_2d_labelled_pcd(self, offset, render="browser"):
+        """[summary]
+
+        Args:
+            offset ([type]): [description]
+            render (str, optional): [description]. Defaults to "browser".
+        """
+        if not self.__data_checker(pcd_2d=True, pcd_label=True):
+            return 
+        pio.renderers.default = render
+        fig = go.Figure()
+        for i in np.arange(np.amax(self._label)):
+            x_cluster = self._x[np.where(self._label == i)]
+            y_cluster = self._y[np.where(self._label == i)]
+            fig.add_trace(go.Scattergl(x=x_cluster, y=y_cluster,
+                mode='markers',
+                marker=dict(
+                    size=3,
+                    colorscale='Viridis',   # choose a colorscale
+                    opacity=0.8)
+            ))
+
+        if self.add_image:
+            # Add images
+            fig.add_layout_image(
+                dict(
+                    source=self.small_img,
+                    xref="x",
+                    yref="y",
+                    x=0 ,
+                    y=offset,
+                    sizex=offset,
+                    sizey=offset,
+                    sizing="stretch",
+                    opacity=0.5,
+                    layer="below")
+            )
+
+        fig.update_yaxes(
+            scaleanchor = "x",
+            scaleratio = 1,
+        )
+        fig.show()
+        
+    def display_3d_pcd(self, render="browser"):
+        """[summary]
+
+        Args:
+            render (str, optional): [description]. Defaults to "browser".
+        """
+        if not self.__data_checker(pcd_3d=True):
+            return 
+        pio.renderers.default = render
+        fig = go.Figure(data=go.Scatter3d(
+        x = self._x,
+        y = self._y,
+        z = self._z,
+        mode='markers',
+        marker=dict(
+            size=3,
+            color=self._z,
+            colorscale='Viridis',   # choose a colorscale
+            opacity=0.8
+        )))
+
+        fig.update_yaxes(
+            scaleanchor = "x",
+            scaleratio = 1
+        )
+        fig.show()
+        
+    def __data_checker(self, pcd_2d=False, pcd_3d=False, pcd_label=False):
+        """[summary]
+
+        Args:
+            pcd_2d (bool, optional): [description]. Defaults to False.
+            pcd_3d (bool, optional): [description]. Defaults to False.
+            pcd_label (bool, optional): [description]. Defaults to False.
+
+        Returns:
+            [type]: [description]
+        """
+        validated = True
+        if pcd_2d:
+            if self.x.size == 0:
+                print("No x axis data")
+                validated = False
+            if self.y.size == 0:
+                print("No y axis data")
+                validated = False
+        
+        if pcd_3d:
+            if self.x.size == 0:
+                print("No x axis data")
+                validated = False
+            if self.y.size == 0:
+                print("No y axis data")
+                validated = False
+            if self.z.size == 0:
+                print("No z axis data")
+                validated = False
+        if pcd_label:
+            if self.label.size == 0:
+                print("No label axis data")
+                validated = False
+        return validated
+    
+    def DBSCAN_2d_tuner(self, esp_range = [500, 1000], min_sample=20, render="browser"):
+        if not self.__data_checker(pcd_2d=True):
+            return 
+        pio.renderers.default = render
+        
+        # Create figure
+        fig = go.Figure()
+
+        bar = Bar('Loading', fill='@', suffix='%(percent)d%% time: %(elapsed)ds', max=int((esp_range[1]-esp_range[0])/100))
+        
+        # Add traces, one for each slider step
+        for esp_step in np.arange(esp_range[0], esp_range[1], 100):
+            bar.next()
+            points = np.vstack((self._x, self._y)).T
+            clustering = OPTICS(max_eps=esp_step, min_samples=min_sample, n_jobs=-1).fit(points)
+            for i in np.arange(np.amax(clustering.labels_)):
+                x_cluster = self._x[np.where(self._label == i)]
+                y_cluster = self._y[np.where(self._label == i)]
+                fig.add_trace(go.Scattergl(x=x_cluster, y=y_cluster,
+                    mode='markers',
+                    marker=dict(
+                        size=3,
+                        colorscale='Viridis',   # choose a colorscale
+                        opacity=0.8)
+                ))
+            
+        bar.finish()
+        
+        # Make 10th trace visible
+        fig.data[0].visible = True
+
+        # Create and add slider
+        steps = []
+        for i in range(len(fig.data)):
+            step = dict(
+                method="update",
+                args=[{"visible": [False] * len(fig.data)},
+                    {"title": "Slider switched to step: " + str(i)}],  # layout attribute
+            )
+            step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
+            steps.append(step)
+
+        sliders = [dict(
+            active=0,
+            currentvalue={"prefix": "Frequency: "},
+            pad={"t": 50},
+            steps=steps
+        )]
+
+        fig.update_layout(
+            sliders=sliders
+        )
+
+        fig.show()
+        
+from processing import ProcessingPipeline
+pipeline = ProcessingPipeline()
+points = pipeline.load_points_from_pkl()
+all_point_x = points[:, 0].T 
+all_point_y = points[:, 1].T
+
+print(all_point_x.shape)
+
+plotter = GraphGUI()
+plotter.x = all_point_x
+plotter.y = all_point_y
+plotter.DBSCAN_2d_tuner(render="browser")
