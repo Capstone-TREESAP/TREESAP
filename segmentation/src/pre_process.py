8895672d6ecac9aadb9d1f934e1ecbfcac95a213
Rainerino <albertyanyy@gmail.com>
2021-04-08 15:10:10 -0700

    update ui

--- segmentation/src/pre_process.py
+++ segmentation/src/pre_process.py
@@ -6,7 +6,8 @@ from pyproj import Proj
 import pickle
 
 # read a las file
-point_cloud = pclpy.read("../../lidar/data/UBC_481_5456_UTM10_SP000-2595_v1.laz", "PointXYZ")
+point_cloud = pclpy.read(
+    "../../lidar/data/2015/UBC_481_5456_UTM10_SP000-2595_v1.laz", "PointXYZ")
 output = "../../lidar/data/UBC_481_5456_UTM10_SP000-2595_v1.pcd"
 writer = pcl.io.PCDWriter()
 writer.writeBinary(output, point_cloud)
@@ -17,14 +18,15 @@ input_data[:, 0] = input_data[:, 0] - 481000
 input_data[:, 1] = input_data[:, 1] - 5456000
 pcd.points = o3d.utility.Vector3dVector(input_data)
 
-UTM_10_PROJ = Proj("+proj=utm +zone=10N, +north +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+UTM_10_PROJ = Proj(
+    "+proj=utm +zone=10N, +north +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
 filename = '../../lidar/data/UBC_481_5456_UTM10_DEM_SP000-2595_v1.xyz'
 xyz = open(filename, "r")
 count = 0
 for line in xyz:
     count += 1
 xyz.close()
-xyz = open(filename,"r")
+xyz = open(filename, "r")
 dem_points = np.ndarray((count, 3))
 count = 0
 for line in xyz:
@@ -47,8 +49,8 @@ downpcd_points[:, 2] = dists
 downpcd.points = o3d.utility.Vector3dVector(downpcd_points)
 
 plane_model, inliers = downpcd.segment_plane(distance_threshold=1.0,
-                                         ransac_n=3,
-                                         num_iterations=1000)
+                                             ransac_n=3,
+                                             num_iterations=1000)
 inlier_cloud = downpcd.select_by_index(inliers)
 ground_diff = downpcd.compute_point_cloud_distance(inlier_cloud)
 ground_diff = np.asarray(ground_diff)
@@ -58,4 +60,3 @@ pcd_without_ground = downpcd.select_by_index(ind)
 with open("no_ground_all.pkl", "wb") as f:
     pickle.dump(np.asarray(pcd_without_ground.points), f)
 print(np.asarray(pcd_without_ground.points).shape)
-
