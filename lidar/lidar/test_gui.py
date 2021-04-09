8ebb2d55e66914551ab779b98b6c30e2994d1730
Rainerino <albertyanyy@gmail.com>
2021-04-09 13:57:56 -0700

    finish GUI rework

--- lidar/lidar/test_gui.py
+++ lidar/lidar/test_gui.py
@@ -5,10 +5,12 @@ from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi("../pipeline.ui", self)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
